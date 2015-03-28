632da7c5b1129346c6aad1178eb0dcb36d1b1767
3DPrintScanner <1988anthonythomas@gmail.com>
2015-03-28 14:43:53 +0100

    stomp still doesnt work?????

--- lib/junction_listener.rb
+++ lib/junction_listener.rb
@@ -2,12 +2,19 @@ require 'stomp'
 
 begin
   # Credentials set here as environment variables
-  @user = ENV["DATAFEEDS_USER"]
-  @password = ENV["DATAFEEDS_PASSWORD"]
+  # @user = ENV["DATAFEEDS_USER"]
+  # @password = ENV["DATAFEEDS_PASSWORD"]
   @key = ENV["DATAFEEDS_KEY"]
   @host = "datafeeds.networkrail.co.uk"
-  @port = 61618
+  @port = 61613
 
+  # puts "Check user"
+  # @user  = CGI::escape(ENV['DATAFEEDS_USER'])
+  @user = 'd3user'
+  @password = 'd3password'
+  @key = 'D36f743bd6-a870-4f9c-9424-aa1b901642b3'
+
+  # puts split
   # Example destination add yours here
   @destination = "/topic/TD_MC_EM_SIG_AREA"
 
