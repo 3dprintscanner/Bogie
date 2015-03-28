14aa8952d040dc820fa25aadc81a1c5c105b2d5b
Eve Barabas <b_seven_e@yahoo.co.uk>
2015-03-28 13:10:52 +0000

    :pencil: add ruby verision, gemfile

---
+++ lib/junction_listener.rb
@@ -0,0 +1,24 @@
+require 'stomp'
+
+begin
+  # Credentials set here as environment variables
+  @user = ENV["DATAFEEDS_USER"]
+  @password = ENV["DATAFEEDS_PASSWORD"]
+  @key = ENV["DATAFEEDS_KEY"]
+  @host = "datafeeds.networkrail.co.uk"
+  @port = 61618
+
+  # Example destination add yours here
+  @destination = "/topic/TD_MC_EM_SIG_AREA"
+
+  puts "Connecting to datafeeds as #{@user} using stomp protocol stomp://#{@host}:#{@port}\n"
+  @connection = Stomp::Connection.open @user, @password, @host, @port, true
+  @connection.subscribe @destination
+
+  while true
+    @msg = @connection.receive
+    puts @msg
+  end
+  @connection.disconnect
+rescue
+end
